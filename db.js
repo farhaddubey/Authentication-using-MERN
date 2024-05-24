660bd8adeb7ee4b425d585d191b4961ddb54ff55
Farhad Dubey <95957352+farhaddubey@users.noreply.github.com>
2024-05-24 11:47:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/"
+const mongoURI = ""
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("Connected to Mongoose successfully");
